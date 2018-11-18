61c15baf4cfe91e52fe9fe875e66e60cf5df3be3
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-17 19:50:08 -0600

    PROCESAR Y TRABAJAR CON DATOS JSON
    Ejercicio realizado en la clase

--- js/script.js
+++ js/script.js
@@ -7,22 +7,38 @@ l.style.display = 'none'
 b.addEventListener('click', evt => {
     l.style.display = 'block'
     const xhr = new XMLHttpRequest()
-    console.log(`Objeto creado : ${xhr.readyState}`)
 
-    xhr.open('GET', 'http://localhost:8080/Ajax/data.html', true)
-    console.log(`Objeto abierto : ${xhr.readyState}`)
+    xhr.open('GET', 'http://localhost:8080/Ajax/json.json', true)
 
     // Qué se debe hacer con la data?
     xhr.addEventListener('load', e => {
-        console.log(e.target)
-        // c.innerHTML = e.target.responseText
-         console.log(`Objeto cargado : ${xhr.readyState}`)
+
+        const data = JSON.parse(e.target.responseText)
+        draw(data)
          l.style.display = 'none'
-        
     })
     //Realice la peticion
     xhr.send()
-    console.log(`Accion solicitado: ${xhr.readyState}`)
 
 
 })
+
+const draw = data => {
+    data.forEach(n => {
+        const container = document.createElement('div')
+        const title = document.createElement('h2')
+        const content = document.createElement('p')
+        const datenew = document.createElement('span')
+
+        title.textContent = n.rojo
+        content.textContent = n.verde
+        datenew.textContent = n.cyan
+
+        container.appendChild(title)
+        container.appendChild(content)
+        container.appendChild(datenew)
+
+        c.appendChild(container)
+
+    });
+}
