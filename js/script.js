1a4fbf65519d09f0dbd67905c1c64c34cf2e9bfa
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-17 19:14:51 -0600

    ESTADO DEL OBJETO XMLHTTPREQUEST
    Ejercico realizadon en clase

--- js/script.js
+++ js/script.js
@@ -3,14 +3,21 @@ const b = document.getElementById('btnLoad')
 
 b.addEventListener('click', evt => {
     const xhr = new XMLHttpRequest()
+    console.log(`Objeto creado : ${xhr.readyState}`)
+
     xhr.open('GET', 'http://localhost:8080/Ajax/data.html', true)
+    console.log(`Objeto abierto : ${xhr.readyState}`)
 
     // Qué se debe hacer con la data?
     xhr.addEventListener('load', e => {
         console.log(e.target)
-        c.innerHTML = e.target.responseText
+        // c.innerHTML = e.target.responseText
+    console.log(`Objeto cargado : ${xhr.readyState}`)
+        
     })
     //Realice la peticion
     xhr.send()
+    console.log(`Accion solicitado: ${xhr.readyState}`)
+
 
 })
