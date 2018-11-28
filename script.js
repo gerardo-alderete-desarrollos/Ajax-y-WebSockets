0ab583182418d2ec4dad3c3fc661a4dc7c0ca9e4
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-28 15:59:13 -0600

    HACIENDO USO DE NUESTRA LIBRERIA

---
+++ script.js
@@ -0,0 +1,55 @@
+const ajax = request => {
+    return new Promise( (resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.open(request.method, request.url, true)
+        xhr.addEventListener('load', e => {
+            resolve(e.target)
+        })
+        xhr.send()
+    } )
+}
+
+
+const showMarvel = async () => {
+    const hash = '00e02946f327fbee15d329d24e6c6a3a'
+    const apiKey = '54e8dba6a3dde372103b682af0265855'
+    const url = `http://gateway.marvel.com/v1/public/characters
+    ?nameStartsWith=iron%20man&limit=20&ts=1&apikey=${apiKey}&hash=${hash}`
+
+    const r = { method: 'GET', url: url}
+    const response = await ajax(r)
+    switch  (response.status) {
+        case 200:
+        console.log(JSON.parse(response.responseText))
+            draw(JSON.parse(response.responseText).data.results)
+            break
+        case 400:
+            setText('Error de cliente ' + response.status)
+            break
+        default:
+            setText('Error desconocido ' + response.status)
+    }
+
+
+}
+
+const draw = data => {
+    const fragment = document.createDocumentFragment()
+    data.forEach(comic => {
+        const container = document.createElement('div')
+        const title = document.createElement('h2')
+        const image = document.createElement('img')
+        
+        title.textContent = comic.name
+        image.src = `${comic.thumbnail.path}/portrait_incredible.${comic.thumbnail.extension}`
+        
+        container.appendChild(title)
+        container.appendChild(img)
+        fragment.appendChild(container)
+
+    })
+
+    myContent.appendChild(fragment)
+}
+
+btn.addEventListener('click', () => {showMarvel()})
